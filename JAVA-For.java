8a18d4cbe70fa1def59be3aa1ef98a6b6a9e129a
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2018-08-14 22:34:22 -0300

    Add files via upload
    for básico em JAVA

---
+++ JAVA-For.java
@@ -0,0 +1,24 @@
+public class NewClass {
+    
+    public static void main (String [] args) {
+        
+       int i;
+       int par;
+       par = 0;
+       
+       for (i=10; i>0;i--){
+       
+       if (i % 2 == 0){
+           par++;
+           continue;
+           
+       }
+      
+       System.out.println("Número de pares"+par);
+       
+       
+        
+        
+       }}}
+    
+
