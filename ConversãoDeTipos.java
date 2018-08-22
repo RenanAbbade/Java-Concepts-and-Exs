00293716e091dfd9ee81f86273fb1e36af54d867
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2018-08-22 13:56:27 -0300

    Add files via upload

---
+++ ConversãoDeTipos.java
@@ -0,0 +1,18 @@
+public class MyClass {
+    
+    public static void main(String args[]) {
+     	int a, b;
+      	double result;
+      
+      	a = 5;
+     	b = 2;
+        String s = "33";
+      
+        result = (double) a/b; /*Casting */
+      	a = Integer.parseInt(s); /*Conversão de uma String para Int */
+        System.out.println(result);
+      	System.out.println(a);
+        
+       
+    }   
+}
