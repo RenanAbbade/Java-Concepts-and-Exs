311cd23f74bacc7ae6b4c3d501f3fe0054bd82d4
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2018-09-06 16:09:04 -0300

    Update JAVA-For.java

--- JAVA-For.java
+++ JAVA-For.java
@@ -21,4 +21,25 @@ public class NewClass {
         
        }}}
     
+public class condicional {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escreva quantos números deseja ter na soma ");
+		int num = sc.nextInt();
+		
+		int  soma = 0;
+		
+		for(int i = 1; i <= num;i++) {
+			System.out.print("valor #"+i+":");
+			int x = sc.nextInt();
+			soma += x;
+		}
+		System.out.println(soma);
+		
+		sc.close();
+		
+		
+	}
+	}
 
