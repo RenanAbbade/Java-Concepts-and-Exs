528c603710d364de18c5a4f0f2de6b183283fa05
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2018-10-07 21:20:59 -0300

    Update OrientacaoObj1.java

--- OrientacaoObj1.java
+++ OrientacaoObj1.java
@@ -1,16 +1,27 @@
 ___________________________________________________________________________________________________________________
 
 package entities;
+package entities;
 
 public class Triangle {
+	//criando os atributos da classe triangulo
 	public double a;
 	public double b;
 	public double c;
+	
+	/* Criando função- public para ser acessada por outras
+	  classes, a segunda palavra double para indicar o retorno 
+	  do valor numérico, void quando a função não retorna, em seguida
+	  o nome da operação, com os parametros, no caso essa função não
+	  recebe parametros */
+	public  double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p-a) * (p-b) * (p-c));
+	}
 
 }
+
 ____________________________________________________________________________________________________________________
-
-
 package application;
 
 import java.util.Locale;
@@ -26,11 +37,12 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		Triangle x, y;
-		//Para que eu possa usar o objeto, para que ele exista, preciso instanciar com o comando new
+		//Para que eu possa usar o objeto, para que ele exista, preciso instanciar com o comando new, criando os objetos x e y derivados da classe triangulo
 		x = new Triangle();
 		y = new Triangle(); 
 		
 		System.out.println("Entre com os tamanhos dos lados do triangulo X: ");
+		//os atributos da classe são herdados pelos objetos, assim sendo, cada objeto x e y, tem os atributos a,b e c
 		x.a = sc.nextDouble();
 		x.b = sc.nextDouble();
 		x.c = sc.nextDouble();
@@ -40,11 +52,9 @@ public class Program {
 		y.c = sc.nextDouble();
 		
 		//Area do triangulo
-		double p = ((x.a + x.b + x.c)/2);
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 		
-		p = ((y.a + y.b + y.c)/2);
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaX = x.area(); //Area do triangulo x será o objeto x.(ponto) nome da função Area que indica chamada da função - ou seja, objeto x chama função da classe triangulo
+		double areaY = y.area();
 		
 		System.out.printf("Area do triangulo X:  %.4f%n",areaX);
 		System.out.printf("Area do triangulo Y:  %.4f%n",areaY);	
@@ -54,15 +64,6 @@ public class Program {
 		}else {
 			System.out.printf("Maior área:Y, com %.4f%n",areaY);
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		
 		sc.close();
 	}
