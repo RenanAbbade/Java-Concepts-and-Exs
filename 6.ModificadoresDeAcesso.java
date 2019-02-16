0745d7a0ca7506f81162444f0d6d3e923d342dba
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2019-02-16 17:26:00 -0200

    Create 6.ModificadoresDeAcesso.java

---
+++ 6.ModificadoresDeAcesso.java
@@ -0,0 +1,10 @@
+/*Modificadores de acesso
+•	Private: o membro só pode ser acessado na própria classe, para ser acessado por outras classes se faz necessário uso de Getts e Setters.
+•	Public: o membro é acessado por todas as classes(ao menos que ele resida em um módulo diferente que não exporte o pacote onde ele está).
+•	Protected: O membro só pode ser acessado no mesmo pacote, bem como em subclasses de pacotes diferentes.
+•	Nada: Ex: “String name” -> Só fica acessível ao mesmo pacote.
+
+*/
+
+public static void main(String[]args){
+}
